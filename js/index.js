c761032be0163abd0ed771c569214a8080dd50f3
Sasuke <zhenlin1993@gmail.com>
2019-06-16 21:08:19 +0800

    add door

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
 var images = [];
 images.push('images/left_top_01.png');
+images.push('images/left_top_02.png');
 images.push('images/bottom_01.png');
 images.push('images/text_01.png');
 images.push('images/text_02.png');
 images.push('images/center_01.png');
+images.push('images/logo.png');
+images.push('images/decorate_01.png');
+images.push('images/decorate_02.png');
+images.push('images/decorate_03.png');
+images.push('images/decorate_04.png');
 displayHideUI('#page_01');
+displayHideUI('#page_02');
 imgLoader(images, function (percentage) {
     var percentT = percentage * 100;
     $('#loader__info').html('Loading ' + (parseInt(percentT)) + '%');
@@ -21,3 +28,10 @@ function displayHideUI(id) {
 function displayShowUI(id) {
     $(id).css("display", "block");
 }
+
+$("#door").click(function() {
+    $("#page_01").fadeOut(1000, function(){
+        displayHideUI("#page_01");
+        displayShowUI("#page_02");
+    });
+});
